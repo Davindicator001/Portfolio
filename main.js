bedcb9a8793810e7b67034e172de3a2891ca0839
victorakande090@gmail.com <victorakande090@gmail.com>
2025-05-01 12:52:03 +0100

    restructuring

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('Hello World!');
+const observer = new IntersectionObserver((entries) => {
+			entries.forEach(entry => {
+				const el = entry.target;
+				if (entry.isIntersecting) {
+					el.classList.add(`animate-${el.dataset.animate}`);
+				} else {
+					el.classList.remove(`animate-${el.dataset.animate}`);
+				}
+			});
+		}, { threshold: 0.1 });
+
+		document.querySelectorAll('.fade-in-section').forEach(el => {
+			observer.observe(el);
+		});
